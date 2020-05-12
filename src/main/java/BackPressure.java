bc1c25fa92f597986dad2d6e1789defe19b6de7a
raymond gitonga <raytosh95@gmail.com>
2020-05-13 02:40:58 +0300

    Use Flowable to immplement automatic backpressure

--- src/main/java/BackPressure.java
+++ src/main/java/BackPressure.java
@@ -1,3 +1,20 @@
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+
 public class BackPressure {
-    
+    public static void main(String[] args) throws InterruptedException {
+        Flowable.range(1, 1000000)
+                .map(e -> {
+                    System.out.println("Produced item is : " + e + " : " + Thread.currentThread().getName());
+                    return e;
+                })
+                .observeOn(Schedulers.io())
+                .subscribe(e -> {
+                    Thread.sleep(100);
+                    System.out.println("Consumed item is: " + e + " : " + Thread.currentThread().getName());
+                });
+
+        Thread.sleep(100000000);
+    }
 }
