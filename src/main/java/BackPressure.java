4bc322ea9a265edfa3ba92d3020376d64f900d8c
raymond gitonga <raytosh95@gmail.com>
2020-05-13 03:01:00 +0300

    Use Flowable to immplement automatic backpressure

--- src/main/java/BackPressure.java
+++ src/main/java/BackPressure.java
@@ -1,6 +1,9 @@
 import io.reactivex.Flowable;
-import io.reactivex.Observable;
 import io.reactivex.schedulers.Schedulers;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class BackPressure {
     public static void main(String[] args) throws InterruptedException {
@@ -10,10 +13,45 @@ public class BackPressure {
                     return e;
                 })
                 .observeOn(Schedulers.io())
-                .subscribe(e -> {
-                    Thread.sleep(100);
-                    System.out.println("Consumed item is: " + e + " : " + Thread.currentThread().getName());
-                });
+                .subscribe(new Subscriber<Integer>() {
+                    Subscription s;
+                    AtomicInteger count = new AtomicInteger(0);
+                               @Override
+                               public void onSubscribe(Subscription s) {
+                                   this.s = s;
+                                   System.out.println("Asking 20 minutes");
+                                   s.request(20);
+                               }
+
+                               @Override
+                               public void onNext(Integer integer) {
+                                   System.out.println("The subscriber cpnsumed : "+ integer);
+
+                                   if (count.getAndIncrement() % 20 == 0){
+                                       System.out.println("Asking for the next 20 items");
+                                       s.request(20);
+                                   }
+
+                                   try {
+                                       Thread.sleep(100);
+                                   } catch (InterruptedException e) {
+                                       e.printStackTrace();
+                                   }
+
+                               }
+
+                               @Override
+                               public void onError(Throwable t) {
+                                   t.printStackTrace();
+                               }
+
+                               @Override
+                               public void onComplete() {
+                                   System.out.println("Completed");
+                               }
+                           }
+
+                );
 
         Thread.sleep(100000000);
     }
